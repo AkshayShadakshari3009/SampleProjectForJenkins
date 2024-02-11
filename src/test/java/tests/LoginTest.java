c82c63533291dec6433a15ea523dc3930ce7fe2e
AkshayShadakshari3009 <AkshayShadakshari@dckap.com>
2024-02-11 14:24:40 +0530

    Initial commit for jenkins testing

---
+++ src/test/java/tests/LoginTest.java
@@ -0,0 +1,13 @@
+package tests;
+
+import org.testng.annotations.Test;
+
+import static java.lang.System.*;
+
+public class LoginTest {
+
+    @Test
+    public void test(){
+        out.println("Inside Login Test");
+    }
+}
---
+++ src/test/java/tests/PaymentTest.java
@@ -0,0 +1,14 @@
+package tests;
+
+import org.testng.annotations.Test;
+
+public class PaymentTest {
+
+
+    @Test
+    public void paymentTest() {
+
+        System.out.println("Inside Payment Test");
+
+    }
+}
